805a0f8f7c299b1571487b3fbc83fb07d3419d44
Tech-A11 <tech-a11@Tech-A11s-MacBook-Air.local>
2018-04-25 14:20:21 +0100

    added homepage test methods

--- features/demo_homepage.feature
+++ features/demo_homepage.feature
@@ -2,3 +2,37 @@ Feature: DemoHomepage
 
 	Scenario: Testing cucumber
 		Given I visit the homepage
+
+	Scenario: Testing visiting checkout cart form homepage
+		Given I visit the homepage
+		When I click on the checkout cart
+
+		Given I visit the homepage
+		When I click on the Product Category button
+
+		Given I visit the homepage
+		When I click on the My Account button
+
+		Given I visit the homepage
+		When I click on the All Product button
+
+		Given I visit the homepage
+		When I click on a Blog post
+
+		Given I visit the homepage
+		When I hover over Products Categories and I click on the categories list option
+
+		Given I visit the homepage
+		When I hover over Products Categories and I click on the iMacs list option
+
+		Given I visit the homepage
+		When I hover over Products Categories and I click on the iPads list option
+
+		Given I visit the homepage
+		When I hover over Products Categories and I click on the iPhones list option
+
+		Given I visit the homepage
+		When I hover over Products Categories and I click on the iPods list option
+
+		Given I visit the homepage
+		When I hover over Products Categories and I click on the MacBooks list option
--- features/lib/pages/demoqa_homepage.rb
+++ features/lib/pages/demoqa_homepage.rb
@@ -4,9 +4,72 @@ class Homepage
 	include Capybara::DSL
 
 	HOMEPAGE_URL = 'http://store.demoqa.com/'
+	PRODUCT_CATEGORY = 'Product Category'
+	CHECKOUT_CART = 'Checkout'
+	FIRST_ITEM_ON_CATEGORY_LIST = 'Accessories'
+	SECOND_ITEM_ON_CATEGORY_LIST = 'iMacs'
+	THIRD_ITEM_ON_CATEGORY_LIST = 'iPads'
+	FOURTH_ITEM_ON_CATEGORY_LIST = 'iPhones'
+	FITH_ITEM_ON_CATEGORY_LIST = 'iPods'
+	SIXTH_ITEM_ON_CATEGORY_LIST = 'MacBooks'
+	MY_ACCOUNT = 'My Account'
+	ALL_PRODUCTS = 'All Product'
+	BLOG_POST_LINK = 'More Details'
 
 	def visit_homepage
 		visit(HOMEPAGE_URL)
+	end
+
+	def click_blog_post
+		 click_link(BLOG_POST_LINK)
+ 	end
+
+	def click_on_product_category
+		click_link(PRODUCT_CATEGORY)
+	end
+
+	def click_product_accessories
+		click_link(FIRST_ITEM_ON_CATEGORY_LIST)
+	end
+
+	def click_on_my_account_button
+		click_link(MY_ACCOUNT)
+	end
+
+	def visit_checkout_cart
+		click_link(CHECKOUT_CART)
+	end
+
+	def click_on_all_products_button
+		click_link(ALL_PRODUCTS)
+	end
+
+	def hover_product_categories
+		find_link(PRODUCT_CATEGORY).hover
+	end
+
+	def click_first_product_on_dropdown_menu
+		click_link(FIRST_ITEM_ON_CATEGORY_LIST)
+	end
+
+	def click_second_product_on_dropdown_menu
+		click_link(SECOND_ITEM_ON_CATEGORY_LIST)
+	end
+
+	def click_third_product_on_dropdown_menu
+		click_link(THIRD_ITEM_ON_CATEGORY_LIST)
+	end
+
+	def click_fourth_product_on_dropdown_menu
+		click_link(FOURTH_ITEM_ON_CATEGORY_LIST)
+	end
+
+	def click_fith_product_on_dropdown_menu
+		click_link(FITH_ITEM_ON_CATEGORY_LIST)
+	end
+
+	def click_sixth_product_on_dropdown_menu
+		click_link(SIXTH_ITEM_ON_CATEGORY_LIST)
 	end
 
 end
